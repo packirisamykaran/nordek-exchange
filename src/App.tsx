86e90da97eaf376078fa41f2d55d590ba4a25ebe
Karan <packirisamykaran@gmail.com>
2024-02-18 18:31:01 +0800

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Wallets from './components/Wallet';
-
+import { ReactComponent as Logo } from './assets/logo1.svg';
+import './app.css';
 
 function App() {
-
-
-
   // States
   // Current chain user is on
-  const [chain, useChain] = useState<"Solana" | "Ethereum">("Solana");
+  const [chain, setChain] = useState<"Solana" | "Ethereum">("Solana");
 
-
-
-
+  const handleChainChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setChain(event.target.value as "Solana" | "Ethereum");
+  };
 
   return (
     <div className="app">
+      <div className="navbar">
+        <Logo className='logo' />
 
-      <Wallets chain={chain} />
+        <Wallets chain={chain} />
+      </div>
 
+
+      <select value={chain} onChange={handleChainChange}>
+        <option value="Solana">Solana</option>
+        <option value="Ethereum">Ethereum</option>
+      </select>
 
     </div>
   );
