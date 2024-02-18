fd7a2085aba15df06ff2a0d1b280286bea781772
Karan <packirisamykaran@gmail.com>
2024-02-18 16:31:58 +0800

    styling and font set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./index.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter, CoinbaseWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
 
+
+// Default styles that can be overridden by your app's styles
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const wallets = [
+  new PhantomWalletAdapter(),
+  new SolflareWalletAdapter(),
+  new CoinbaseWalletAdapter(),
+  new TorusWalletAdapter(),
+
+];
+
+const network = WalletAdapterNetwork.Mainnet; // Change this to 'testnet' or 'devnet' as neede
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConnectionProvider endpoint={clusterApiUrl(network)}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>
 );
 
